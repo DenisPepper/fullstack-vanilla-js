5900c11aceddb52d34c15fd1906e3652486c4b80
DenisPepper <thech@yandex.ru>
2024-10-06 19:19:22 +0300

    Добавит черновик для тестирования

---
+++ test/web.test.js
@@ -0,0 +1,7 @@
+import { describe, it } from 'node:test';
+
+describe('web app test suite', () => {
+  it('Добавит новую строку в таблицу при валидных даных формы', async () => {
+    console.log('working...');
+  });
+});
