321c373348ec3020e8c8db080f5bfb44fdc2b0a9
DenisPepper <thech@yandex.ru>
2024-10-06 20:41:53 +0300

    Добавит замоканный объект с методами

--- test/web.test.js
+++ test/web.test.js
@@ -1,7 +1,38 @@
-import { describe, it } from 'node:test';
+import { describe, it, before } from 'node:test';
+import Controller from '../src/shared/controller.js';
+import View from '../src/platforms/web/view.js';
+
+// замокает window
+globalThis.window = {};
+
+function getDocument(mock) {
+  // замокает document
+  globalThis.document = {
+    createElement: mock.fn((name) => ({
+      classList: {
+        add: mock.fn((name) => {}),
+      },
+    })),
+    querySelector: mock.fn((selector) => ({
+      reset: mock.fn(() => {}),
+      appendChild: mock.fn((element) => {}),
+      addEventListener: mock.fn((name, fn) => {}),
+    })),
+  };
+
+  return globalThis.document;
+}
 
 describe('web app test suite', () => {
-  it('Добавит новую строку в таблицу при валидных даных формы', async () => {
-    console.log('working...');
+  let _controller;
+
+  before(() => {});
+
+  it('Добавит новую строку в таблицу при валидных даных формы', async (context) => {
+    const document = getDocument(context.mock);
+    Controller.init({
+      view: new View(),
+    });
+    document.querySelector.mock;
   });
 });
--- ui/index.js
+++ ui/index.js
@@ -4,7 +4,7 @@ const platform = globalThis.window ? 'web' : 'console';
 
 const { default: View } = await import(`./../src/platforms/${platform}/view.js`);
 
-// Factory design pattern`  
+// Factory design pattern`
 Controller.init({
   view: new View(),
 });
